6f4cf73cadf22e2230e105534184af95af61086b
Janzkockel <janzkockel44@gmail.com>
2025-09-09 08:30:09 +0200

    adding draw feature

--- static/someoneWon.js
+++ static/someoneWon.js
@@ -1,47 +1,65 @@
 import { replay, new_game } from "./replay_anotherGame.js";
+import { gameIdOutput } from "./script.js";
 
 export function someoneWon(winner, GameStatus, myGameId, playerId) {
 
-    tl.disabled = true;
-    tm.disabled = true;
-    tr.disabled = true;
+    if (winner === "draw") {
+        gameIdOutput.innerText = "It's a draw!";
+        gameIdOutput.style.fontSize = "3.5vw";
+        gameIdOutput.style.fontWeight = "600";
+        gameIdOutput.style.color = "#ffffff";
+        gameIdOutput.style.fontFamily = "'Arial Black', Arial, sans-serif";
+        gameIdOutput.style.letterSpacing = "0.1vw";
+        gameIdOutput.style.margin = "0";
 
-    ml.disabled = true;
-    mm.disabled = true;
-    mr.disabled = true;
+        replay.style.visibility = "visible";
+        new_game.style.visibility = "visible";
 
-    bl.disabled = true;
-    bm.disabled = true;
-    br.disabled = true;
+        if (GameStatus[myGameId][playerId + "-replay"] === "u") {
+            replay.style.background = "#ffffff10";
+        }
+    }
+    else {
+        tl.disabled = true;
+        tm.disabled = true;
+        tr.disabled = true;
 
-    gameIdOutput.innerText = "Winner is:";
-    gameIdOutput.style.fontSize = "3.5vw";
-    gameIdOutput.style.fontWeight = "600";
-    gameIdOutput.style.color = "#ffffff";
-    gameIdOutput.style.fontFamily = "'Arial Black', Arial, sans-serif";
-    gameIdOutput.style.letterSpacing = "0.1vw";
-    gameIdOutput.style.margin = "0";
+        ml.disabled = true;
+        mm.disabled = true;
+        mr.disabled = true;
 
-    winnerIdOutput.innerText = winner;
-    winnerIdOutput.style.fontSize = "5vw";
-    winnerIdOutput.style.fontWeight = "700";
-    winnerIdOutput.style.color = "#ffcc33";
-    winnerIdOutput.style.fontFamily = "'Arial Black', Arial, sans-serif";
-    winnerIdOutput.style.letterSpacing = "0.2vw";
-    winnerIdOutput.style.fontStyle = "italic";
-    winnerIdOutput.style.textShadow = `
+        bl.disabled = true;
+        bm.disabled = true;
+        br.disabled = true;
+
+        gameIdOutput.innerText = "Winner is:";
+        gameIdOutput.style.fontSize = "3.5vw";
+        gameIdOutput.style.fontWeight = "600";
+        gameIdOutput.style.color = "#ffffff";
+        gameIdOutput.style.fontFamily = "'Arial Black', Arial, sans-serif";
+        gameIdOutput.style.letterSpacing = "0.1vw";
+        gameIdOutput.style.margin = "0";
+
+        winnerIdOutput.innerText = winner;
+        winnerIdOutput.style.fontSize = "5vw";
+        winnerIdOutput.style.fontWeight = "700";
+        winnerIdOutput.style.color = "#ffcc33";
+        winnerIdOutput.style.fontFamily = "'Arial Black', Arial, sans-serif";
+        winnerIdOutput.style.letterSpacing = "0.2vw";
+        winnerIdOutput.style.fontStyle = "italic";
+        winnerIdOutput.style.textShadow = `
     0 0 0.2vw #fff, 
     0 0 0.4vw #fff, 
     0 0 0.6vw #ffcc33, 
     0 0 0.8vw #ffcc33
 `;
-    winnerIdOutput.style.margin = "0";
+        winnerIdOutput.style.margin = "0";
 
-    replay.style.visibility = "visible";
-    new_game.style.visibility = "visible";
-    
-    if(GameStatus[myGameId][playerId + "-replay"] === "u"){
-        replay.style.background = "#ffffff10";
+        replay.style.visibility = "visible";
+        new_game.style.visibility = "visible";
+
+        if (GameStatus[myGameId][playerId + "-replay"] === "u") {
+            replay.style.background = "#ffffff10";
+        }
     }
-
 }
--- static/winAlgorithm.js
+++ static/winAlgorithm.js
@@ -1,21 +1,36 @@
 export function checkWin(gameStatus, myGameId) {
-    for(let row=0; row<=2; row++){
-        if(gameStatus[myGameId].gameStatus[row][0] !== 'u' && gameStatus[myGameId].gameStatus[row][0] === gameStatus[myGameId].gameStatus[row][1] && gameStatus[myGameId].gameStatus[row][1] === gameStatus[myGameId].gameStatus[row][2]){
+    let movesCount = 0;
+
+    for (let row = 0; row <= 2; row++) {
+        if (gameStatus[myGameId].gameStatus[row][0] !== 'u' && gameStatus[myGameId].gameStatus[row][0] === gameStatus[myGameId].gameStatus[row][1] && gameStatus[myGameId].gameStatus[row][1] === gameStatus[myGameId].gameStatus[row][2]) {
             return gameStatus[myGameId].gameStatus[row][0];
         }
     }
-    for(let col=0; col<=2; col++){
-        if(gameStatus[myGameId].gameStatus[0][col] !== 'u' && gameStatus[myGameId].gameStatus[0][col] === gameStatus[myGameId].gameStatus[1][col] && gameStatus[myGameId].gameStatus[1][col] === gameStatus[myGameId].gameStatus[2][col]){
+    for (let col = 0; col <= 2; col++) {
+        if (gameStatus[myGameId].gameStatus[0][col] !== 'u' && gameStatus[myGameId].gameStatus[0][col] === gameStatus[myGameId].gameStatus[1][col] && gameStatus[myGameId].gameStatus[1][col] === gameStatus[myGameId].gameStatus[2][col]) {
             return gameStatus[myGameId].gameStatus[0][col];
         }
     }
 
-    if(gameStatus[myGameId].gameStatus[0][0] !== 'u' && gameStatus[myGameId].gameStatus[0][0] === gameStatus[myGameId].gameStatus[1][1] && gameStatus[myGameId].gameStatus[1][1] === gameStatus[myGameId].gameStatus[2][2]){
+    if (gameStatus[myGameId].gameStatus[0][0] !== 'u' && gameStatus[myGameId].gameStatus[0][0] === gameStatus[myGameId].gameStatus[1][1] && gameStatus[myGameId].gameStatus[1][1] === gameStatus[myGameId].gameStatus[2][2]) {
         return gameStatus[myGameId].gameStatus[0][0];
     }
-    if(gameStatus[myGameId].gameStatus[0][2] !== 'u' && gameStatus[myGameId].gameStatus[0][2] === gameStatus[myGameId].gameStatus[1][1] && gameStatus[myGameId].gameStatus[1][1] === gameStatus[myGameId].gameStatus[2][0]){
+    if (gameStatus[myGameId].gameStatus[0][2] !== 'u' && gameStatus[myGameId].gameStatus[0][2] === gameStatus[myGameId].gameStatus[1][1] && gameStatus[myGameId].gameStatus[1][1] === gameStatus[myGameId].gameStatus[2][0]) {
         return gameStatus[myGameId].gameStatus[0][2];
     }
+
+    for (let row = 0; row <= 2; row++) {
+        for (let col = 0; col <= 2; col++) {
+            if (gameStatus[myGameId].gameStatus[row][col] != 'u') {
+                movesCount++;
+            }
+        }
+    }
+
+    if (movesCount === 9) {
+        return "draw";
+    }
+
     return "noWinner";
 
 } 
